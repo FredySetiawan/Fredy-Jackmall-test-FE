d87722e689f49435aef31d69a6145eb720581ad9
FredySetiawan <113951583+FredySetiawan@users.noreply.github.com>
2022-10-12 18:01:20 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Delevery from "./Pages/Delivery";
+import Delivery from "./Pages/Delivery";
 
 
 function App() {
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
         <Routes>
-          <Route path="/" index element={<Delevery />} />
+          <Route path="/" index element={<Delivery />} />
         </Routes>
     </BrowserRouter>
   );
